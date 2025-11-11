6a20fad22478833b35225ae2a06e2db32a64b168
Ambikesh Kushwaha <147277970+Shawnforste27@users.noreply.github.com>
2025-11-11 11:50:57 +0530

    Update multer.js

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 // Define storage configuration (in memory)
 const storage = multer.memoryStorage();
 
-// ✅ Correct usage: call `.single("file")` after `multer()`
+
 const uploadFile = multer({ storage }).single("file");
 
 module.exports = uploadFile;
