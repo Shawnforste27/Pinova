faf33000bdfd9d1e2d74a0b7c7642936617cbae7
Ambikesh Kushwaha <147277970+Shawnforste27@users.noreply.github.com>
2025-11-11 12:28:22 +0530

    Refactor middleware order in server.js
    Reordered middleware usage for better organization.

--- server/server.js
+++ server/server.js
@@ -21,21 +21,15 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-app.use(express.json());
-app.use(cookieParser());
-
-
-
 app.use(
   cors({
-    origin: [
-      "https://pinova-client-v2.netlify.app",
-      "http://localhost:5173",
-    ],
+    origin: ["https://pinova-client-v2.netlify.app", "http://localhost:5173"],
     credentials: true,
   })
 );
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 connectDb();
